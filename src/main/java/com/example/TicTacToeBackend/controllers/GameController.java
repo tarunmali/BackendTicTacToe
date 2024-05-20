6940406f107b90d493472a4acee6f0135c5c2ecb
Tarun Mali <tarunmalibackup2@gmail.comm>
2024-05-20 21:23:35 +0530

    should work local

--- src/main/java/com/example/TicTacToeBackend/controllers/GameController.java
+++ src/main/java/com/example/TicTacToeBackend/controllers/GameController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
     @GetMapping
     public String game() {
-        return "Hello, local!";
+        return "Hello, github!";
     }
 }
