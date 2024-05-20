b6bf47e2fac771ca6766753792e80fd1972475b8
Tarun Mali <tarunmalibackup2@gmail.comm>
2024-05-20 23:33:47 +0530

    should work local

--- src/main/java/com/example/TicTacToeBackend/controllers/GameController.java
+++ src/main/java/com/example/TicTacToeBackend/controllers/GameController.java
@@ -3,13 +3,16 @@ package com.example.TicTacToeBackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/game")
+//@RequestMapping("/simple")
+@RequestMapping(value="/game", method= RequestMethod.GET)
+//@RequestMapping(value="/greet")
 public class GameController {
     @GetMapping
-    public String game() {
-        return "Hello, god!";
+    public String greet() {
+        return "Hello, World!";
     }
 }
