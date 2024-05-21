22ad8f034dc9039969244b05c5fd9fbd6447cba4
Tarun Mali <tarunmalibackup2@gmail.comm>
2024-05-21 12:24:21 +0530

    Implemented register feature

--- src/main/java/com/example/TicTacToeBackend/game/Game.java
+++ src/main/java/com/example/TicTacToeBackend/game/Game.java
@@ -3,6 +3,8 @@ package com.example.TicTacToeBackend.game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class Game {
     @Autowired
@@ -10,7 +12,20 @@ public class Game {
     private Player player1, player2,nextPlayer,winner;
 
     public int registerNewPlayer(){
-        return 1;
+        Random random = new Random();
+        int id;
+        if(player1==null){
+            player1=new Player(random.nextInt(1,100),Move.ZERO);
+            id=player1.getId();
+        }
+        else if(player2==null){
+            player2=new Player(random.nextInt(1,100),Move.CROSS);
+            id=player2.getId();
+        }
+        else{
+            throw new IllegalStateException("Registration is closed");
+        }
+        return id;
     }
 
 }
--- src/main/java/com/example/TicTacToeBackend/game/Player.java
+++ src/main/java/com/example/TicTacToeBackend/game/Player.java
@@ -1,4 +1,13 @@
 package com.example.TicTacToeBackend.game;
 
 public class Player {
+    private final int id;
+    private final Move move;
+    public Player(int id, Move move){
+        this.id=id;
+        this.move=move;
+    }
+    public int getId() {
+        return id;
+    }
 }
